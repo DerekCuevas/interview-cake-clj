9aab23b1f4ce706c26973fc7bc8ae7b2753ff9a9
Derek Cuevas <dccuevas2@gmail.com>
2016-05-21 16:33:39 -0700

    added faster O(n) solution for product of all other numbers

--- product-of-all-other-numbers/src/product_of_all_other_numbers/core.clj
+++ product-of-all-other-numbers/src/product_of_all_other_numbers/core.clj
@@ -1,6 +1,19 @@
 (ns product-of-all-other-numbers.core
   (:gen-class))
 
-;; O(n^2) solutions
+;; O(n^2) solutions - using division
 (defn product-of-others [nums]
   (map #(/ (reduce * nums) %) nums))
+
+(defn- products-before [nums]
+  (reduce (fn [acc n]
+            (conj acc (* n (last acc))))
+          [1]
+          (butlast nums)))
+
+;; O(5n) ~ O(n) solution - without using division
+(defn product-of-others-fast [nums]
+  (if (empty? nums)
+    '()
+    (map * (products-before nums)
+           (reverse (products-before (reverse nums))))))
