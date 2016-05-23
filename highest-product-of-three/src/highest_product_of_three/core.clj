8bf581b5996b593bc8f0bddfc20c3dbd0c355632
Derek Cuevas <dccuevas2@gmail.com>
2016-05-23 11:21:00 -0700

    added first solutions for highest product of three

--- highest-product-of-three/src/highest_product_of_three/core.clj
+++ highest-product-of-three/src/highest_product_of_three/core.clj
@@ -1,5 +1,28 @@
 (ns highest-product-of-three.core
   (:gen-class))
 
-(defn highest-product-of-three [nums]
+(defn- largest-k [k]
+  (fn [largest n]
+    (let [min-largest (and (not-empty largest) (apply min largest))]
+      (cond
+        (< (count largest) k) (conj largest n)
+        (> n min-largest) (conj (disj largest min-largest) n)
+        :else largest))))
+
+(defn highest-product-of-k-positive-ints [k nums]
+  (apply * (reduce (largest-k k) #{} nums)))
+
+(defn highest-product-of-three-positive-ints
+  "O(n) solution - without negative numbers"
+  [nums]
+  (highest-product-of-k-positive-ints 3 nums))
+
+;; maxs < k
+;; mins < k => if count of min < k - 1 is even? comp product of maxs to product of mins - 1 and largest max
+(defn highest-product-of-k [k nums]
   nil)
+
+(defn highest-product-of-three
+  "O(n) solution - with negative numbers"
+  [nums]
+  (highest-product-of-k 3 nums))
--- highest-product-of-three/test/highest_product_of_three/core_test.clj
+++ highest-product-of-three/test/highest_product_of_three/core_test.clj
@@ -2,6 +2,10 @@
   (:require [clojure.test :refer :all]
             [highest-product-of-three.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest highest-product-of-three-test
+  (testing "highest product of three"
+    (is (= (highest-product-of-three-positive-ints [5 8 7 2 3 5]) 280))
+    (is (= (highest-product-of-three-positive-ints [1 2 3]) 6))
+    (is (= (highest-product-of-three-positive-ints [6 4 5 2]) 120))
+    (is (= (highest-product-of-three-positive-ints [3 5 7 1 2 4]) 140))
+    (is (= (highest-product-of-three-positive-ints [5 8]) 40))))
