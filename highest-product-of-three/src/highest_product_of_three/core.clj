1dabde3ab8355d8b3f033d9c1531c081c6aa035a
Derek Cuevas <dccuevas2@gmail.com>
2016-05-23 14:51:21 -0700

    added solution for highest product of three with negatives

--- highest-product-of-three/src/highest_product_of_three/core.clj
+++ highest-product-of-three/src/highest_product_of_three/core.clj
@@ -1,6 +1,10 @@
 (ns highest-product-of-three.core
   (:gen-class))
 
+;; assumptions made:
+;; - colls are unique
+;; - count of coll is always greater than k
+
 (defn- select-k
   "Selects k items from a collection where (select selected) is not nil
   and (replace? n) is true. Will select blindly if k is greater than
@@ -32,21 +36,20 @@
   [nums]
   (highest-product-of-k-positive-ints 3 nums))
 
-;; maxs < k
-;; mins < k => if count of min < k - 1 is even?
-;;             comp product of maxs to product of mins - 1 and largest max
-
-;; four conds
-;; if (mins.len is even?) {}
-
-;; TODO: implement this
-(defn highest-product-of-k [k nums]
-  (let [negative-mins (filter neg? (select-k-mins k nums))
-        maxs (filter? pos? (select-k-maxs k nums))]
-    (if (even? k)
-      (max (apply * mins) (apply * maxs)))))
-
 (defn highest-product-of-three
   "O(n) solution - with negative numbers"
   [nums]
-  (highest-product-of-k 3 nums))
+  (let [min-negatives (filter neg? (select-k-mins 2 nums))
+        maxs (select-k-maxs 3 nums)
+        product-of-maxs (apply * maxs)]
+    (if (= (count min-negatives) 2)
+      (max product-of-maxs
+           (* (reduce max maxs) (apply * min-negatives)))
+      product-of-maxs)))
+
+;; TODO: implement this
+#_((defn highest-product-of-k [k nums]
+  (let [negative-mins (filter neg? (select-k-mins k nums))
+        maxs (filter pos? (select-k-maxs k nums))]
+    (if (even? k)
+      (max (apply * mins) (apply * maxs))))))
