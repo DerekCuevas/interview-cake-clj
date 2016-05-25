c9bf487903e50c9fdee7db8b121648c390e396da
Derek Cuevas <dccuevas2@gmail.com>
2016-05-25 09:55:51 -1000

    refactor to apple stocks

--- apple-stocks/src/apple_stocks/core.clj
+++ apple-stocks/src/apple_stocks/core.clj
@@ -1,14 +1,13 @@
 (ns apple-stocks.core
   (:gen-class))
 
-;; FIXME: is there a built in equivalent to this?
-(def ^:private infinity 1000000000)
-
 (defn- update-min-max [{:keys [min-price max-profit]} price]
   {:min-price (min price min-price)
    :max-profit (max (- price min-price) max-profit)})
 
-(defn get-max-profit [stock-prices]
+(defn get-max-profit
+  "O(n) time solution - returns max potential profit from set of stock prices."
+  [stock-prices]
   (:max-profit (reduce update-min-max
-                       {:max-profit 0 :min-price infinity}
-                       stock-prices)))
+                       {:max-profit 0 :min-price (first stock-prices)}
+                       (rest stock-prices))))
