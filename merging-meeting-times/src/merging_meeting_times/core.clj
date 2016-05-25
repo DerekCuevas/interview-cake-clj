1730e0e359799962222009c834cb86c2df82929c
Derek Cuevas <dccuevas2@gmail.com>
2016-05-24 15:32:14 -1000

    added doc strs

--- merging-meeting-times/src/merging_meeting_times/core.clj
+++ merging-meeting-times/src/merging_meeting_times/core.clj
@@ -1,13 +1,17 @@
 (ns merging-meeting-times.core
   (:gen-class))
 
-(defn- merge-if-overlap [meeting-a meeting-b]
+(defn- merge-if-overlap
+  "Merges meetings if overlap, nil otherwise."
+  [meeting-a meeting-b]
   (let [[earlier later] (sort-by :start [meeting-a meeting-b])]
     (when (>= (earlier :end) (later :start))
       {:start (earlier :start)
        :end (max (earlier :end) (later :end))})))
 
-(defn merge-meeting-times [meeting-times]
+(defn merge-meeting-times
+  "O(nlgn) soluting - sorting input meeting times."
+  [meeting-times]
   (let [sorted-meeting-times (sort-by :start meeting-times)]
     (reduce (fn [merged meeting]
               (if-let [merged-meetings (merge-if-overlap (last merged) meeting)]
