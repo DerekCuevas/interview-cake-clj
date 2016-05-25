f03132fadd55b437ba8303397aff242fb2c74b1c
Derek Cuevas <dccuevas2@gmail.com>
2016-05-25 07:10:22 -1000

    small name changes to rectangular overlap

--- rectangular-love/src/rectangular_love/core.clj
+++ rectangular-love/src/rectangular_love/core.clj
@@ -1,7 +1,9 @@
 (ns rectangular-love.core
   (:gen-class))
 
-(defn- overlapping-range [range-a range-b]
+(defn- intersection
+  "Returns intersection of ranges, nil if no overlap."
+  [range-a range-b]
   (let [[under over] (sort-by :start [range-a range-b])]
     (when (>= (under :end) (over :start))
       {:start (max (under :start) (over :start))
@@ -14,14 +16,14 @@
   {:start (rect :x) :end (+ (rect :x) (rect :width))})
 
 (defn- find-vertical-overlap [rect-a rect-b]
-  (overlapping-range (rect->vertical-range rect-a)
-                     (rect->vertical-range rect-b)))
+  (intersection (rect->vertical-range rect-a)
+                (rect->vertical-range rect-b)))
 
 (defn- find-horizontal-overlap [rect-a rect-b]
-  (overlapping-range (rect->horizontal-range rect-a)
-                     (rect->horizontal-range rect-b)))
+  (intersection (rect->horizontal-range rect-a)
+                (rect->horizontal-range rect-b)))
 
-(defn- length [{:keys [start end]}]
+(defn- range->length [{:keys [start end]}]
   (- end start))
 
 (defn rectangular-overlap
@@ -32,5 +34,5 @@
     (when (and vertical-overlapping-range horizontal-overlapping-range)
       {:x (horizontal-overlapping-range :start)
        :y (vertical-overlapping-range :start)
-       :width (length horizontal-overlapping-range)
-       :height (length vertical-overlapping-range)})))
+       :width (range->length horizontal-overlapping-range)
+       :height (range->length vertical-overlapping-range)})))
