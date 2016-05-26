46f6d2803b36aace083a0c487c8969b60b2ddd36
Derek Cuevas <dccuevas2@gmail.com>
2016-05-26 13:22:48 -1000

    refactored binary tree

--- balanced-binary-tree/src/balanced_binary_tree/binary_tree.clj
+++ balanced-binary-tree/src/balanced_binary_tree/binary_tree.clj
@@ -4,23 +4,17 @@
 (defn node [value]
   {:value value :left nil :right nil})
 
-(defn set-left [root n]
-  (assoc root :left n))
-
-(defn set-right [root n]
-  (assoc root :right n))
-
-(defn- height [root pred]
+(defn max-height [root]
   (if (nil? root)
     0
-    (inc (pred (height (root :left) pred)
-               (height (root :right) pred)))))
-
-(defn max-height [root]
-  (height root max))
+    (inc (max (max-height (root :left))
+              (max-height (root :right))))))
 
 (defn min-height [root]
-  (height root min))
+  (if (nil? root)
+    0
+    (inc (min (min-height (root :left))
+              (min-height (root :right))))))
 
 (defn insert [root value]
   (loop [current-node root
