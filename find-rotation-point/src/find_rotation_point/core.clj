6f05710954b46fe1658c71edb34a53a9c1877520
Derek Cuevas <dccuevas2@gmail.com>
2016-05-28 17:25:57 -1000

    added faster solution for find rotation point

--- find-rotation-point/src/find_rotation_point/core.clj
+++ find-rotation-point/src/find_rotation_point/core.clj
@@ -1,13 +1,29 @@
 (ns find-rotation-point.core
   (:gen-class))
 
+(def ^:private gt? (comp pos? compare))
+(def ^:private lt? (comp neg? compare))
+
 (defn find-rotation-point
   "O(n) time solution."
   [words]
   (loop [idx 1]
-    (if (pos? (compare (words (dec idx)) (words idx)))
+    (if (gt? (words (dec idx)) (words idx))
       idx
       (recur (inc idx)))))
 
-(defn find-rotation-point-fast [words]
-  -1)
+(defn- midpoint [start end]
+  (+ (int (/ (- end start) 2)) start))
+
+(defn find-rotation-point-fast
+  "O(lgn) - binary search like solution."
+  [[first-word :as words]]
+  (loop [start 0
+         end (count words)]
+    (let [mid (midpoint start end)
+          mid-word (words mid)]
+      (cond
+        ;; TODO: double check for edge conds
+        (<= (- end start) 1) end
+        (gt? mid-word first-word) (recur mid end)
+        (lt? mid-word first-word) (recur start mid)))))
