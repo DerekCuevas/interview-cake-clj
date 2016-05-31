70815b1482be3580c8635e1dc7369c6dce028933
Derek Cuevas <dccuevas2@gmail.com>
2016-05-31 13:53:13 -1000

    refactored greedy solution - cake thief

--- cake-thief/src/cake_thief/core.clj
+++ cake-thief/src/cake_thief/core.clj
@@ -1,14 +1,17 @@
 (ns cake-thief.core
   (:gen-class))
 
+(defn- steel-cakes [{:keys [capacity-left max-value] :as totals} {:keys [weight value]}]
+  (let [can-take (int (/ capacity-left weight))]
+    (if (>= can-take 0)
+      {:capacity-left (- capacity-left (* weight can-take))
+       :max-value (+ max-value (* value can-take))}
+      totals)))
+
 (defn max-duffel-bag-value-greedy
-  "O(nlgn) time solution - close and greedy approximation"
+  "O(nlgn) time solution - greedy approximation"
   [cakes capacity]
   (let [sorted-by-ratio (sort-by #(/ (:value %) (:weight %)) > cakes)]
-    ((reduce (fn [{:keys [capacity-left max-value]} {:keys [weight value]}]
-              (let [can-take (int (/ capacity-left weight))]
-                (if (>= can-take 0)
-                  {:capacity-left (- capacity-left (* weight can-take))
-                   :max-value (+ max-value (* value can-take))})))
-            {:capacity-left capacity :max-value 0}
-            sorted-by-ratio) :max-value)))
+    ((reduce steel-cakes
+             {:capacity-left capacity :max-value 0}
+             sorted-by-ratio) :max-value)))
