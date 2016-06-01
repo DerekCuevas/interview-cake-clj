bd583877086a999b755fd4d00d501508a4742ce6
Derek Cuevas <dccuevas2@gmail.com>
2016-05-31 17:43:10 -1000

    added dynamic, bottom up solution for cake thief

--- cake-thief/src/cake_thief/core.clj
+++ cake-thief/src/cake_thief/core.clj
@@ -13,3 +13,24 @@
     ((reduce steel-cakes
              {:capacity-left capacity :max-value 0}
              sorted-by-ratio) :max-value)))
+
+(defn- max-value-for-current-capacity [max-value-at-capacity current-capacity]
+  (fn [max-value {:keys [weight value]}]
+    (if (>= current-capacity weight)
+      (max max-value
+           (+ (max-value-at-capacity (- current-capacity weight)) value))
+      max-value)))
+
+;; FIXME: clean up names
+(defn max-duffel-bag-value
+  "O(n*k) time solution, where k = capacity - dynamic and bottom up"
+  [cakes capacity]
+  (loop [max-value-at-capacity (vec (repeat (inc capacity) 0))
+         current-capacity 1]
+    (if (> current-capacity capacity)
+      (max-value-at-capacity capacity)
+      (recur (assoc max-value-at-capacity
+                    current-capacity
+                    (reduce (max-value-for-current-capacity max-value-at-capacity
+                                                            current-capacity) 0 cakes))
+             (inc current-capacity)))))
--- cake-thief/test/cake_thief/core_test.clj
+++ cake-thief/test/cake_thief/core_test.clj
@@ -3,7 +3,19 @@
             [cake-thief.core :refer :all]))
 
 (def cakes-a [{:weight 7 :value 160} {:weight 3 :value 90} {:weight 2 :value 15}])
+(def cakes-b [{:weight 1 :value 30} {:weight 50 :value 200}])
+(def cakes-c [{:weight 3 :value 40} {:weight 5 :value 70}])
 
 (deftest max-duffel-bag-value-greedy-test
   (testing "Should return max monetary value duffel bag can hold."
-    (is (= (max-duffel-bag-value-greedy cakes-a 20) 555))))
+    (is (= (max-duffel-bag-value-greedy cakes-a 20) 555))
+    (is (= (max-duffel-bag-value-greedy cakes-b 100) 3000))
+    (is (= (max-duffel-bag-value-greedy cakes-c 8) 110))))
+    ;; (is (= (max-duffel-bag-value-greedy cakes-c 9) 120)))) ;; edge case
+
+(deftest max-duffel-bag-value-greedy-test
+  (testing "Should return max monetary value duffel bag can hold."
+    (is (= (max-duffel-bag-value cakes-a 20) 555))
+    (is (= (max-duffel-bag-value cakes-b 100) 3000))
+    (is (= (max-duffel-bag-value cakes-c 8) 110))
+    (is (= (max-duffel-bag-value cakes-c 9) 120))))
