7456a146bfada6ad2a39c6544db4df722bd8fe5b
Derek Cuevas <dccuevas2@gmail.com>
2016-06-01 09:56:08 -1000

    added very basic tests for delete node

--- delete-node/test/delete_node/core_test.clj
+++ delete-node/test/delete_node/core_test.clj
@@ -1,7 +1,15 @@
 (ns delete-node.core-test
   (:require [clojure.test :refer :all]
-            [delete-node.core :refer :all]))
+            [delete-node.core :refer :all]
+            [delete-node.list :as list]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def list-a (list/coll->list [5 3 9 6 1]))
+(def list-b (list/coll->list [2 4 6]))
+
+(def removed-a {:value 3, :next {:value 9, :next {:value 6, :next {:value 1, :next nil}}}})
+(def removed-b {:value 4, :next {:value 6, :next nil}})
+
+(deftest delete-node-test
+  (testing "should delete node from list - hard to test without mutation..."
+    (is (= (delete list-a) removed-a))
+    (is (= (delete list-b) removed-b))))
