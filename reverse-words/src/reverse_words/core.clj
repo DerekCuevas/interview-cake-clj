a8a21acaae0b5d4332d56903a8556682b5f13d20
Derek Cuevas <dccuevas2@gmail.com>
2016-06-03 09:36:15 -1000

    added solution for reverse words

--- reverse-words/src/reverse_words/core.clj
+++ reverse-words/src/reverse_words/core.clj
@@ -1,5 +1,9 @@
 (ns reverse-words.core
+  (:require [clojure.string :as str])
   (:gen-class))
 
-(defn reverse-words [s]
-  s)
+;; This problem gets much more challenging in-place with mutable strings
+(defn reverse-words
+  "O(n) time solution - using built in functions"
+  [s]
+  (str/join #" " (reverse (str/split s #" "))))
