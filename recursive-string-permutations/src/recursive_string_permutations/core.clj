5504ffa3840c04cf5efad0a4c96a825039b1eef4
Derek Cuevas <dccuevas2@gmail.com>
2016-06-05 12:34:54 -1000

    added solution for recursive string  permutations

--- recursive-string-permutations/src/recursive_string_permutations/core.clj
+++ recursive-string-permutations/src/recursive_string_permutations/core.clj
@@ -1,5 +1,21 @@
 (ns recursive-string-permutations.core
+  (:require [clojure.string :as str])
   (:gen-class))
 
+(defn- cat-join [char]
+  (fn [coll]
+    (str/join (concat coll (str char)))))
+
+(defn- other-chars [s idx]
+  (str/join (concat (subs s 0 idx)
+                    (subs s (inc idx) (count s)))))
+
 (defn permutations [s]
-  #{})
+  (if (= (count s) 1)
+    #{s}
+    (reduce-kv (fn [permutations-so-far idx char]
+                 (into permutations-so-far
+                       (map (cat-join char)
+                            (permutations (other-chars s idx)))))
+               #{}
+               (vec s))))
