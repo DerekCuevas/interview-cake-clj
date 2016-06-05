3a7753c79af7233c3fd74c616066c7e842e95454
Derek Cuevas <dccuevas2@gmail.com>
2016-06-05 07:26:16 -1000

    added second solution to permutation palindrome

--- permutation-palindrome/src/permutation_palindrome/core.clj
+++ permutation-palindrome/src/permutation_palindrome/core.clj
@@ -1,7 +1,7 @@
 (ns permutation-palindrome.core
   (:gen-class))
 
-(defn odd-occurrences [s]
+(defn- odd-occurrences [s]
   (reduce (fn [counts char]
             (if (contains? counts char)
               (disj counts char)
@@ -13,3 +13,8 @@
   "O(n) time & space solution - counting occurrences with a set."
   [s]
   (<= (count (odd-occurrences s)) 1))
+
+(defn permutation-palindrome-better?
+  "O(n) time & space solution - counting odd-occurrences w/frequencies fn."
+  [s]
+  (<= (count (filter odd? (vals (frequencies s)))) 1))
