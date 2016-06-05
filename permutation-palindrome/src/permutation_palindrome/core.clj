5fae5cb563cd0bdb9f6ff54352e1c3f565511037
Derek Cuevas <dccuevas2@gmail.com>
2016-06-04 18:33:09 -1000

    refactored permutaion palindrome

--- permutation-palindrome/src/permutation_palindrome/core.clj
+++ permutation-palindrome/src/permutation_palindrome/core.clj
@@ -1,15 +1,15 @@
 (ns permutation-palindrome.core
   (:gen-class))
 
+(defn odd-occurrences [s]
+  (reduce (fn [counts char]
+            (if (contains? counts char)
+              (disj counts char)
+              (conj counts char)))
+          #{}
+          s))
+
 (defn permutation-palindrome?
   "O(n) time & space solution - counting occurrences with a set."
   [s]
-  (let [odd-counts (reduce (fn [counts char]
-                             (if (contains? counts char)
-                               (disj counts char)
-                               (conj counts char)))
-                           #{}
-                           s)]
-    (if (even? (count s))
-      (empty? odd-counts)
-      (= 1 (count odd-counts)))))
+  (<= (count (odd-occurrences s)) 1))
