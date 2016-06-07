c140a75eb1fa2f02d97f00837d6ac78983aa0628
Derek Cuevas <dccuevas2@gmail.com>
2016-06-07 06:50:39 -1000

    cleaning up

--- the-stolen-breakfast-drone/src/the_stolen_breakfast_drone/core.clj
+++ the-stolen-breakfast-drone/src/the_stolen_breakfast_drone/core.clj
@@ -4,12 +4,13 @@
 (defn find-unique
   "O(n) time & space solution - using a set"
   [ids]
-  (first (reduce (fn [unique-ids id]
-                   (if (contains? unique-ids id)
-                     (disj unique-ids id)
-                     (conj unique-ids id)))
-                 #{}
-                 ids)))
+  (first (reduce
+           (fn [unique-ids id]
+             (if (contains? unique-ids id)
+               (disj unique-ids id)
+               (conj unique-ids id)))
+           #{}
+           ids)))
 
 (defn find-unique-bitwise
   "O(n) time solution - using bitwise xor"
