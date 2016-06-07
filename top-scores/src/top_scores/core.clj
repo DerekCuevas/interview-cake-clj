b4357456ce62b558e33b57e75a39a39f0e0cce4b
Derek Cuevas <dccuevas2@gmail.com>
2016-06-06 19:15:19 -1000

    cleaning up top scores

--- top-scores/src/top_scores/core.clj
+++ top-scores/src/top_scores/core.clj
@@ -1,7 +1,7 @@
 (ns top-scores.core
   (:gen-class))
 
-(defn- count-items [coll max-value]
+(defn- value-counts [coll max-value]
   (reduce
     (fn [counts value]
       (assoc counts value (inc (counts value))))
@@ -9,10 +9,10 @@
     coll))
 
 (defn counting-sort
-  "O(n) time & O(k) space solution, where k = max-value."
+  "O(n) time & O(n + k) space solution, where k = max-value."
   [coll max-value]
   (reduce-kv
     (fn [sorted value count]
       (into sorted (take count (repeat value))))
     []
-    (count-items coll max-value)))
+    (value-counts coll max-value)))
