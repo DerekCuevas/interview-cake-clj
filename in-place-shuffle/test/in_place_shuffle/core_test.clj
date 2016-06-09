444a48aee8ecdd30826ba3c452c842aaba0f4918
Derek Cuevas <dccuevas2@gmail.com>
2016-06-08 18:43:56 -1000

    cleaning up

--- in-place-shuffle/test/in_place_shuffle/core_test.clj
+++ in-place-shuffle/test/in_place_shuffle/core_test.clj
@@ -7,8 +7,8 @@
 (def vector-c [6 7 2 9 8 1 3])
 (def vector-d [23 88 6 54 23 40 91 66 78 20 12 102])
 
-(deftest a-test
-  (testing "FIXME, I fail."
+(deftest fisher-yates-shuffle-test
+  (testing "Shuffles items in vector, preserves items and count of vector."
     (is (= (sort (fisher-yates-shuffle vector-a)) (sort vector-a)))
     (is (= (sort (fisher-yates-shuffle vector-b)) (sort vector-b)))
     (is (= (sort (fisher-yates-shuffle vector-c)) (sort vector-c)))
