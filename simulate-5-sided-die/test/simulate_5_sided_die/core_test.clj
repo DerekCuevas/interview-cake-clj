f66b5784710aa56d429ad633f9761b8b9629c32d
Derek Cuevas <dccuevas2@gmail.com>
2016-06-10 14:36:33 -1000

    refactored tests for simulate 5 sided die

--- simulate-5-sided-die/test/simulate_5_sided_die/core_test.clj
+++ simulate-5-sided-die/test/simulate_5_sided_die/core_test.clj
@@ -2,11 +2,27 @@
   (:require [clojure.test :refer :all]
             [simulate-5-sided-die.core :refer :all]))
 
+(def ^:private SAMPLE_SIZE 10000)
+(def ^:private EXPECTED_DISTRIBUTION (/ SAMPLE_SIZE 5))
+(def ^:private MARGIN_OF_ERROR 100)
+
+(defn- rand5-seq [n]
+  (reduce
+    (fn [rs _] (conj rs (rand5)))
+    []
+    (range n)))
+
+(defn- within-margin-of-error? [freq]
+  (and (>= freq (- EXPECTED_DISTRIBUTION MARGIN_OF_ERROR))
+       (<= freq (+ EXPECTED_DISTRIBUTION MARGIN_OF_ERROR))))
+
+(defn- within-range? [value]
+  (and (>= value 1) (<= value 5)))
+
 (deftest rand5-test
-  (testing "should return random number between 1 and 5"
-    (loop [n 5000]
-      (let [rand (rand5)]
-        (is (<= rand 5))
-        (is (>= rand 1))
-        (when-not (zero? (dec n))
-          (recur (dec n)))))))
+  (testing "probablity distribution"
+    (is (every? within-margin-of-error?
+                (vals (frequencies (rand5-seq SAMPLE_SIZE))))))
+  (testing "range check"
+    (is (every? within-range?
+                (rand5-seq SAMPLE_SIZE)))))
