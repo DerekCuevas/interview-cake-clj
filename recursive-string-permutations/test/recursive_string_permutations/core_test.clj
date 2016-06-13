11d33f67cfe8509f319b59cbf8c94027e0f4ec41
Derek Cuevas <dccuevas2@gmail.com>
2016-06-13 08:01:43 -1000

    cleaning up tests for recursive string permutations

--- recursive-string-permutations/test/recursive_string_permutations/core_test.clj
+++ recursive-string-permutations/test/recursive_string_permutations/core_test.clj
@@ -2,26 +2,27 @@
   (:require [clojure.test :refer :all]
             [recursive-string-permutations.core :refer :all]))
 
-(def str-a "a")
-(def permutations-a #{"a"})
+(def permutations-a
+  #{"a"})
 
-(def str-b "ab")
-(def permutations-b #{"ab" "ba"})
+(def permutations-b
+  #{"ab" "ba"})
 
-(def str-c "abc")
-(def permutations-c #{"abc" "acb"
-                      "bac" "bca"
-                      "cab" "cba"})
+(def permutations-c
+  #{"abc" "acb"
+    "bac" "bca"
+    "cab" "cba"})
 
-(def str-d "abcd")
-(def permutations-d #{"abcd" "abdc" "acbd" "acdb" "adbc" "adcb"
-                      "badc" "bacd" "bcda" "bcad" "bdca" "bdac"
-                      "cabd" "cadb" "cbad" "cbda" "cdab" "cdba"
-                      "dacb" "dabc" "dbca" "dbac" "dcba" "dcab"})
+(def permutations-d
+  #{"abcd" "abdc" "acbd" "acdb" "adbc" "adcb"
+    "badc" "bacd" "bcda" "bcad" "bdca" "bdac"
+    "cabd" "cadb" "cbad" "cbda" "cdab" "cdba"
+    "dacb" "dabc" "dbca" "dbac" "dcba" "dcab"})
 
 (deftest permutations-test
   (testing "should return a set of all permutations of an input string"
-    (is (= (permutations str-a) permutations-a))
-    (is (= (permutations str-b) permutations-b))
-    (is (= (permutations str-c) permutations-c))
-    (is (= (permutations str-d) permutations-d))))
+    (is (= (permutations "") #{""}))
+    (is (= (permutations "a") permutations-a))
+    (is (= (permutations "ab") permutations-b))
+    (is (= (permutations "abc") permutations-c))
+    (is (= (permutations "abcd") permutations-d))))
