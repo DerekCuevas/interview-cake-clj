6d9f13dbbaccd59289a949d6ac02ec2ede99d5ae
Derek Cuevas <dccuevas2@gmail.com>
2016-06-14 12:21:20 -1000

    added test case to product of all other numbers

--- product-of-all-other-numbers/test/product_of_all_other_numbers/core_test.clj
+++ product-of-all-other-numbers/test/product_of_all_other_numbers/core_test.clj
@@ -27,5 +27,6 @@
   (testing "should map coll to the product of every integer except the integer at that index"
     (is (= (product-of-others-fast [1 2 3 4 5]) '(120 60 40 30 24)))
     (is (= (product-of-others-fast [1 7 3 4]) '(84 12 28 21)))
+    (is (= (product-of-others-fast [1 0 7 3 4]) '(0 84 0 0 0)))
     (is (= (product-of-others-fast [7 2 3 5]) '(30 105 70 42)))
     (is (= (product-of-others-fast [3 2]) '(2 3)))))
