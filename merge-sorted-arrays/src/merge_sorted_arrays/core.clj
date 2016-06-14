d7fc52e9bde0e943d4ac95f1939f517bdcdea70b
Derek Cuevas <dccuevas2@gmail.com>
2016-06-13 17:07:37 -1000

    added solution for merge sorted arrays

--- merge-sorted-arrays/src/merge_sorted_arrays/core.clj
+++ merge-sorted-arrays/src/merge_sorted_arrays/core.clj
@@ -2,4 +2,19 @@
   (:gen-class))
 
 (defn merge-colls [coll-a coll-b]
-  '())
+  (loop [idx-a 0
+         idx-b 0
+         merged []]
+    (let [a (get coll-a idx-a)
+          b (get coll-b idx-b)]
+      (cond
+        (and (nil? a) (nil? b))
+          merged
+        (nil? a)
+          (recur idx-a (inc idx-b) (conj merged b))
+        (nil? b)
+          (recur (inc idx-a) idx-b (conj merged a))
+        (< a b)
+          (recur (inc idx-a) idx-b (conj merged a))
+        :else
+          (recur idx-a (inc idx-b) (conj merged b))))))
--- merge-sorted-arrays/test/merge_sorted_arrays/core_test.clj
+++ merge-sorted-arrays/test/merge_sorted_arrays/core_test.clj
@@ -2,15 +2,15 @@
   (:require [clojure.test :refer :all]
             [merge-sorted-arrays.core :refer :all]))
 
-(def coll-a '(1 2 3))
-(def coll-b '(3 4 7 9 13))
-(def coll-c '(1 2 5 23 54 67 89 123 456 789))
-(def coll-d (filter even? (range 1000)))
-(def coll-e (filter odd? (range 1000)))
-(def coll-f (range 1000))
+(def coll-a [1 2 3])
+(def coll-b [3 4 7 9 13])
+(def coll-c [1 2 5 23 54 67 89 123 456 789])
+(def coll-d (vec (filter even? (range 1000))))
+(def coll-e (vec (filter odd? (range 1000))))
+(def coll-f (vec (range 1000)))
 
-(def edge-a '())
-(def edge-b '(5))
+(def edge-a [])
+(def edge-b [5])
 
 (deftest merge-colls-test
   (testing "edge cases"
@@ -18,6 +18,6 @@
     (is (= (merge-colls edge-a edge-b) edge-b)))
   (testing "should merge sorted collections."
     (is (= (merge-colls coll-a edge-a) coll-a))
-    (is (= (merge-colls coll-a coll-b) (concat coll-a coll-b)))
-    (is (= (merge-colls coll-b coll-c) '(1 2 3 4 5 7 9 13 23 54 67 89 123 456 789)))
+    (is (= (merge-colls coll-a coll-b) (vec (concat coll-a coll-b))))
+    (is (= (merge-colls coll-b coll-c) [1 2 3 4 5 7 9 13 23 54 67 89 123 456 789]))
     (is (= (merge-colls coll-d coll-e) coll-f))))
