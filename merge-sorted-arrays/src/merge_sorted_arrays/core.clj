9dac91582b70764d907f17725c26a834ac837ac5
Derek Cuevas <dccuevas2@gmail.com>
2016-06-13 17:16:58 -1000

    cleaning up solution for merge sorted arrays

--- merge-sorted-arrays/src/merge_sorted_arrays/core.clj
+++ merge-sorted-arrays/src/merge_sorted_arrays/core.clj
@@ -1,20 +1,19 @@
 (ns merge-sorted-arrays.core
   (:gen-class))
 
-(defn merge-colls [coll-a coll-b]
+(defn- lt? [a b]
+  (and (some? a) (neg? (compare a b))))
+
+(defn merge-vectors [vec-a vec-b]
   (loop [idx-a 0
          idx-b 0
          merged []]
-    (let [a (get coll-a idx-a)
-          b (get coll-b idx-b)]
+    (let [a (get vec-a idx-a)
+          b (get vec-b idx-b)]
       (cond
         (and (nil? a) (nil? b))
           merged
-        (nil? a)
-          (recur idx-a (inc idx-b) (conj merged b))
-        (nil? b)
-          (recur (inc idx-a) idx-b (conj merged a))
-        (< a b)
+        (or (nil? b) (lt? a b))
           (recur (inc idx-a) idx-b (conj merged a))
         :else
           (recur idx-a (inc idx-b) (conj merged b))))))
--- merge-sorted-arrays/test/merge_sorted_arrays/core_test.clj
+++ merge-sorted-arrays/test/merge_sorted_arrays/core_test.clj
@@ -2,22 +2,22 @@
   (:require [clojure.test :refer :all]
             [merge-sorted-arrays.core :refer :all]))
 
-(def coll-a [1 2 3])
-(def coll-b [3 4 7 9 13])
-(def coll-c [1 2 5 23 54 67 89 123 456 789])
-(def coll-d (vec (filter even? (range 1000))))
-(def coll-e (vec (filter odd? (range 1000))))
-(def coll-f (vec (range 1000)))
+(def vec-a [1 2 3])
+(def vec-b [3 4 7 9 13])
+(def vec-c [1 2 5 23 54 67 89 123 456 789])
+(def vec-d (vec (filter even? (range 1000))))
+(def vec-e (vec (filter odd? (range 1000))))
+(def vec-f (vec (range 1000)))
 
 (def edge-a [])
 (def edge-b [5])
 
-(deftest merge-colls-test
+(deftest merge-vectors-test
   (testing "edge cases"
-    (is (= (merge-colls edge-a edge-a) edge-a))
-    (is (= (merge-colls edge-a edge-b) edge-b)))
+    (is (= (merge-vectors edge-a edge-a) edge-a))
+    (is (= (merge-vectors edge-a edge-b) edge-b)))
   (testing "should merge sorted collections."
-    (is (= (merge-colls coll-a edge-a) coll-a))
-    (is (= (merge-colls coll-a coll-b) (vec (concat coll-a coll-b))))
-    (is (= (merge-colls coll-b coll-c) [1 2 3 4 5 7 9 13 23 54 67 89 123 456 789]))
-    (is (= (merge-colls coll-d coll-e) coll-f))))
+    (is (= (merge-vectors vec-a edge-a) vec-a))
+    (is (= (merge-vectors vec-a vec-b) (vec (concat vec-a vec-b))))
+    (is (= (merge-vectors vec-b vec-c) [1 2 3 4 5 7 9 13 23 54 67 89 123 456 789]))
+    (is (= (merge-vectors vec-d vec-e) vec-f))))
