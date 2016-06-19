1bd5915db2553d5345f1b977a17205ffa769b766
Derek Cuevas <dccuevas2@gmail.com>
2016-06-18 21:00:36 -1000

    slight refactor to merge sorted arrays - destructuring halfs

--- merge-sorted-arrays/src/merge_sorted_arrays/core.clj
+++ merge-sorted-arrays/src/merge_sorted_arrays/core.clj
@@ -7,12 +7,12 @@
   (loop [coll-a coll-a
          coll-b coll-b
          merged []]
-    (let [a (first coll-a)
-          b (first coll-b)]
+    (let [[a & as] coll-a
+          [b & bs] coll-b]
       (cond
         (and (empty? coll-a) (empty? coll-b))
           merged
         (or (nil? b) (and (not (nil? a)) (< a b)))
-          (recur (rest coll-a) coll-b (conj merged a))
+          (recur as coll-b (conj merged a))
         :else
-          (recur coll-a (rest coll-b) (conj merged b))))))
+          (recur coll-a bs (conj merged b))))))
