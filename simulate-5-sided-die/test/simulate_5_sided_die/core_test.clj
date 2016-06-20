ac2c8d71346cc14d28068f73dc01f40d3e4c5bb6
Derek Cuevas <dccuevas2@gmail.com>
2016-06-19 20:56:02 -1000

    slight refactor to simulate n sided die tests -> using const metadata

--- simulate-5-sided-die/test/simulate_5_sided_die/core_test.clj
+++ simulate-5-sided-die/test/simulate_5_sided_die/core_test.clj
@@ -2,20 +2,20 @@
   (:require [clojure.test :refer :all]
             [simulate-5-sided-die.core :refer :all]))
 
-(def ^:private MIN 1)
-(def ^:private MAX 5)
-(def ^:private SAMPLE_SIZE 10000)
+(def ^:const MIN 1)
+(def ^:const MAX 5)
+(def ^:const SAMPLE_SIZE 10000)
 
-(def ^:private EXPECTED_FREQUENCY (* SAMPLE_SIZE (/ MIN MAX)))
-(def ^:private LIMIT_FREQUENCY (* SAMPLE_SIZE (/ (inc MIN) MAX)))
-(def ^:private MARGIN_OF_ERROR (/ (- LIMIT_FREQUENCY EXPECTED_FREQUENCY) 16))
+(def ^:const EXPECTED_FREQUENCY (* SAMPLE_SIZE (/ MIN MAX)))
+(def ^:const LIMIT_FREQUENCY (* SAMPLE_SIZE (/ (inc MIN) MAX)))
+(def ^:const MARGIN_OF_ERROR (/ (- LIMIT_FREQUENCY EXPECTED_FREQUENCY) 16))
 
-(defn- within-margin-of-error? [freq]
+(defn within-margin-of-error? [freq]
   (<= (- EXPECTED_FREQUENCY MARGIN_OF_ERROR)
       freq
       (+ EXPECTED_FREQUENCY MARGIN_OF_ERROR)))
 
-(defn- within-range? [value]
+(defn within-range? [value]
   (<= MIN value MAX))
 
 (deftest rand5-test
