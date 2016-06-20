b89561f4c7a3948c56cdc58def4a8552a6387923
Derek Cuevas <dccuevas2@gmail.com>
2016-06-19 20:38:51 -1000

    cleaning up tests for top scores

--- top-scores/test/top_scores/core_test.clj
+++ top-scores/test/top_scores/core_test.clj
@@ -3,21 +3,24 @@
             [top-scores.core :refer :all]))
 
 (defn ordered? [coll]
-  (apply <= coll))
+  (if (<= (count coll) 1)
+    true
+    (apply <= coll)))
 
 (def coll-a (shuffle [5 3 4 8]))
 (def coll-b (shuffle [45 78 2 4 9 99 100 23 17]))
-(def coll-c (shuffle [4 49 15 2 2 3 15 17 9 13 23 45]))
-(def coll-d (shuffle [678 812 13 100 245 346 45 98 99 956 100]))
+(def coll-c (shuffle [4 49 15 2 2 3 15 17 9 13 23 45 4 13]))
+(def coll-d (shuffle [678 812 13 100 245 346 45 98 99 956 100 13 812 812]))
+(def coll-e (shuffle [1 1 1 3 3 3 3 4 4 5 6 6 6 6 6]))
 
 (deftest counting-sort-test
   (testing "edge cases"
-    (is (counting-sort [] 0) [])
+    (is (ordered? (counting-sort [] 0)))
     (is (ordered? (counting-sort [1] 1)))
-    (is (ordered? (counting-sort [5] 5)))
-    (is (ordered? (counting-sort [2 3] 3))))
+    (is (ordered? (counting-sort [1 2] 2))))
   (testing "returns sorted collection"
     (is (ordered? (counting-sort coll-a 10)))
     (is (ordered? (counting-sort coll-b 100)))
     (is (ordered? (counting-sort coll-c 50)))
-    (is (ordered? (counting-sort coll-d 1000)))))
+    (is (ordered? (counting-sort coll-d 1000)))
+    (is (ordered? (counting-sort coll-e 6)))))
