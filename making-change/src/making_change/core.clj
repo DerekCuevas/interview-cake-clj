8e29a04f00c24e0496acb4189601b655ab51b36b
Derek Cuevas <dccuevas2@gmail.com>
2016-06-19 17:14:41 -1000

    added solution for making change

--- making-change/src/making_change/core.clj
+++ making-change/src/making_change/core.clj
@@ -1,5 +1,19 @@
 (ns making-change.core
   (:gen-class))
 
-(defn ways-to-make-amount [amount denominations]
-  0)
+(defn ways-to-make-amount
+  "O(n * m) time and O(n) space solution, where m = # of denominations."
+  [amount denominations]
+  ((reduce
+     (fn [ways-of-doing-n-cents coin]
+       (loop [higher-amount coin
+              ways-of-doing-n-cents ways-of-doing-n-cents]
+         (if (> higher-amount amount)
+           ways-of-doing-n-cents
+           (recur (inc higher-amount)
+                  (assoc ways-of-doing-n-cents
+                         higher-amount
+                         (+ (ways-of-doing-n-cents higher-amount)
+                            (ways-of-doing-n-cents (- higher-amount coin))))))))
+     (vec (conj (replicate amount 0) 1))
+     denominations) amount))
