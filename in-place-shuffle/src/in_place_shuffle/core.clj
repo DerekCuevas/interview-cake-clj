e9c48a9ddfe3f3d3afa4eb9371d7ece14a5375d5
Derek Cuevas <dccuevas2@gmail.com>
2016-06-21 17:08:34 -1000

    cleaning up fisher yates shuffle

--- in-place-shuffle/src/in_place_shuffle/core.clj
+++ in-place-shuffle/src/in_place_shuffle/core.clj
@@ -7,12 +7,11 @@
 (defn fisher-yates-shuffle
   "O(n) time solution - can't do this in-place ~ but can use similar algorithm."
   [arr]
-  (loop [idx 0
-         shuffled arr]
-    (if (= idx (count arr))
-      shuffled
-      (let [rand-idx (random idx (dec (count arr)))]
-        (recur (inc idx)
-               (-> shuffled
-                   (assoc rand-idx (shuffled idx))
-                   (assoc idx (shuffled rand-idx))))))))
+  (reduce
+   (fn [shuffled idx]
+     (let [rand-idx (random idx (dec (count arr)))]
+       (-> shuffled
+           (assoc rand-idx (shuffled idx))
+           (assoc idx (shuffled rand-idx)))))
+   arr
+   (range (count arr))))
