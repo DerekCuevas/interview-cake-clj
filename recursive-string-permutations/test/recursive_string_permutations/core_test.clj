bfb89e1afdd6c4408b9c78fcd9e78bf08d54410f
Derek Cuevas <dccuevas2@gmail.com>
2016-07-08 13:24:42 -1000

    added tests for longer permutations - recursive string permutations

--- recursive-string-permutations/test/recursive_string_permutations/core_test.clj
+++ recursive-string-permutations/test/recursive_string_permutations/core_test.clj
@@ -19,6 +19,14 @@
     "cabd" "cadb" "cbad" "cbda" "cdab" "cdba"
     "dacb" "dabc" "dbca" "dbac" "dcba" "dcab"})
 
+(def long-str-a "abcdef")
+(def long-str-b "abcdefgh")
+(def long-permutations-a (permutations long-str-a))
+(def long-permutations-b (permutations long-str-b))
+
+(defn factorial [n]
+  (reduce * (range 1 (inc n))))
+
 (deftest permutations-test
   (testing "edge cases"
     (is (= (permutations "") #{""}))
@@ -26,4 +34,9 @@
   (testing "returns set of all permutations of an input string"
     (is (= (permutations "ab") permutations-b))
     (is (= (permutations "abc") permutations-c))
-    (is (= (permutations "abcd") permutations-d))))
+    (is (= (permutations "abcd") permutations-d)))
+  (testing "longer permutations"
+    (is (set? long-permutations-a))
+    (is (set? long-permutations-b))
+    (is (= (count long-permutations-a) (factorial (count long-str-a))))
+    (is (= (count long-permutations-b) (factorial (count long-str-b))))))
